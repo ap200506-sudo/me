869c92c9547b380d73c5bad2fa85ed7f61a4df7e
ap200506-sudo <ap200506@gmail.com>
2025-11-23 20:59:11 +0530

    Time: 2 ms (67.37%), Space: 41.4 MB (91.78%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int j=0;
+        int i=0;
+        if(t.length()==0){
+            if(s.length()==0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        while(i<s.length() && j<t.length()){
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+            
+
+           
+            
+
+        }
+        return i == s.length();
+        
+        
+    }
+}
