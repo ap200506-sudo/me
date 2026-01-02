86b57881ca2af495bd70d44ee9d68a6a95566b71
ap200506-sudo <ap200506@gmail.com>
2026-01-02 11:37:57 +0530

    Time: 0 ms (100%), Space: 43.7 MB (7.68%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,18 +1,20 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-
-        for (int i = n - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++;  // simple increment
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
                 return digits;
             }
-            digits[i] = 0;  // set to 0 and carry to next digit
+            digits[i]=0;
         }
-
-        // If all digits were 9, you need a new array like [1, 0, 0, 0...]
-        int[] result = new int[n + 1];
-        result[0] = 1;
+        
+        int result[] = new int[n+1];
+        
+        result[0]=1;
         return result;
+        
+        
+        
     }
 }
