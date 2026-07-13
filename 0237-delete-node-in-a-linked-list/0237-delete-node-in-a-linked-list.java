dc7ff4168dc7fb4b4d650575f695e5cac28396b4
ap200506-sudo <ap200506@gmail.com>
2026-07-13 07:29:16 +0530

    Time: 0 ms (100%), Space: 45.4 MB (23.45%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,9 +8,14 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
+        while(node.next.next!=null){
+            node.val=node.next.val;
+            node=node.next;
+        }
         node.val=node.next.val;
-        node.next=node.next.next;
+        node.next=null;
 
+        node=node.next;
         
     }
 }
