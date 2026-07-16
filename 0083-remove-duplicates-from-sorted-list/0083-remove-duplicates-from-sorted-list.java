0a10cf471421e62028c9cd6769f9f4a90537fd84
ap200506-sudo <ap200506@gmail.com>
2026-07-16 07:46:14 +0530

    Time: 0 ms (100%), Space: 45.4 MB (69.52%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -10,17 +10,18 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+
         ListNode curr=head;
-       
-        while(curr!=null && curr.next!=null  ){
+        if(curr==null || curr.next==null){
+            return curr;
+        }
+        while(curr.next!=null){
             if(curr.val==curr.next.val){
                 curr.next=curr.next.next;
             }
-            else {
-                    curr=curr.next;
-                }
-            
-
+            else{
+                curr=curr.next;
+            }
         }
         return head;
         
